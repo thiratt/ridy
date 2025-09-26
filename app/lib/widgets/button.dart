43d9ea3f63b4f91c52a09445825024abc6f7c178
thiratt <thiratcha7@gmail.com>
2025-09-26 19:21:36 +0700

    feat(buttons): add PrimaryButton and OutlinedAppButton widgets

---
+++ app/lib/widgets/button.dart
@@ -0,0 +1,62 @@
+import 'package:app/themes/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const PrimaryButton({super.key, required this.text, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: FilledButton(
+        onPressed: onPressed,
+        style: FilledButton.styleFrom(
+          backgroundColor: AppTheme.light.colorScheme.primary,
+          foregroundColor: AppTheme.light.colorScheme.onPrimary,
+          side: BorderSide(color: AppTheme.light.colorScheme.onPrimary),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(text, style: const TextStyle(fontSize: 16)),
+      ),
+    );
+  }
+}
+
+class OutlinedAppButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const OutlinedAppButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 56,
+      child: OutlinedButton(
+        onPressed: onPressed,
+        style: OutlinedButton.styleFrom(
+          foregroundColor: AppTheme.light.colorScheme.onSurface,
+          side: BorderSide(
+            color: AppTheme.light.colorScheme.onSurface,
+            width: 1.5,
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12),
+          ),
+        ),
+        child: Text(text, style: const TextStyle(fontSize: 16)),
+      ),
+    );
+  }
+}
