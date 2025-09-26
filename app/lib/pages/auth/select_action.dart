612692a34c34040022bbd9430d874f483d36a3a8
thiratt <thiratcha7@gmail.com>
2025-09-26 20:00:33 +0700

    feat(select-action): redesign SelectActionPage layout with images and buttons

--- app/lib/pages/auth/select_action.dart
+++ app/lib/pages/auth/select_action.dart
@@ -1,3 +1,5 @@
+import 'package:app/pages/auth/login.dart';
+import 'package:app/widgets/button.dart';
 import 'package:flutter/material.dart';
 
 class SelectActionPage extends StatefulWidget {
@@ -11,8 +13,46 @@ class _SelectActionPageState extends State<SelectActionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Select Action Page')),
-      body: const Center(child: Text('Welcome to the Select Action Page!')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Image.asset("assets/images/cover.png"),
+              Column(
+                children: [
+                  Image.asset(
+                    "assets/images/logo_black.png",
+                    width: 120,
+                    height: 120,
+                  ),
+                  const Text(
+                    "ยินดีต้อนรับสู่แอป Ridy",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 0, 16, 24),
+            child: Column(
+              spacing: 4,
+              children: [
+                PrimaryButton(
+                  text: "เข้าสู่ระบบ",
+                  onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  ),
+                ),
+                OutlinedAppButton(text: "สมัครสมาชิก", onPressed: () {}),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
