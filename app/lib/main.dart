6f4d91dc4c429f107a5736ed01209138631c0c8a
thiratt <thiratcha7@gmail.com>
2025-10-01 04:53:46 +0700

    refactor: rename MyApp to RidyApplication for consistency

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,11 +3,11 @@ import 'package:app/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RidyApplication());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RidyApplication extends StatelessWidget {
+  const RidyApplication({super.key});
 
   @override
   Widget build(BuildContext context) {
