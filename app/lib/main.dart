06d1d2bacc394787bcf9394027f82ef1637723cb
thiratt <thiratcha7@gmail.com>
2025-10-23 21:30:14 +0700

    feat: add named routes

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,10 @@
+import 'package:app/models/user_signup_draft.dart';
+import 'package:app/pages/auth/login/page.dart';
+import 'package:app/pages/auth/signup/page.dart';
+import 'package:app/pages/auth/signup/rider/page.dart';
+import 'package:app/pages/auth/signup/user/add_address.dart';
+import 'package:app/pages/auth/signup/user/page.dart';
+import 'package:app/pages/auth/signup/user/select_address.dart';
 import 'package:app/pages/onboarding.dart';
 import 'package:app/themes/app_theme.dart';
 import 'package:flutter/material.dart';
@@ -15,7 +22,31 @@ class RidyApplication extends StatelessWidget {
       title: 'Ridy',
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      home: const OnBoardingPage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const OnBoardingPage(),
+        '/auth/login': (context) => const LoginPage(),
+        '/auth/signup': (context) => const SignupSelection(),
+        '/auth/signup/user': (context) => const UserSignupPage(),
+        '/auth/signup/user/address/select': (context) =>
+            const SelectLocationPage(),
+        '/auth/signup/rider': (context) => const RiderSignupPage(),
+      },
+      onGenerateRoute: (settings) => MaterialPageRoute(
+        builder: (context) {
+          if (settings.name == '/auth/signup/user/address') {
+            final args = settings.arguments as Map<String, dynamic>?;
+            final draft = args != null && args['draft'] is UserSignupDraft
+                ? args['draft'] as UserSignupDraft
+                : null;
+            if (draft != null) {
+              return UserSignupAddAddressPage(draft: draft);
+            }
+          }
+          return const OnBoardingPage();
+        },
+        settings: settings,
+      ),
     );
   }
 }
