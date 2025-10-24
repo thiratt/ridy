385116358936689717e3cc44580d853e8256c86f
thiratt <thiratcha7@gmail.com>
2025-10-24 22:48:54 +0700

    feat: add api routing for client

--- app/lib/config/api_constants.dart
+++ app/lib/config/api_constants.dart
@@ -9,6 +9,13 @@ sealed class ApiRoute {
   static const checkPhoneNumber = _RidyRoute('/account/check-phone');
   static const getAllUsers = _RidyRoute('/account');
   static const register = _RidyRoute('/account/signup');
+
+  static const userDeliveries = _RidyRoute('/delivery/user/');
+  static const sentDeliveries = _RidyRoute('/delivery/sent/');
+  static const receivedDeliveries = _RidyRoute('/delivery/received/');
+  static const userDeliveriesByStatus = _RidyRoute(
+    '/delivery/user/{v}/status/{v}',
+  );
 }
 
 class _RidyRoute extends ApiRoute {
@@ -18,4 +25,13 @@ class _RidyRoute extends ApiRoute {
 class ApiConstants {
   static String baseUrl = getEnv('BASE_URL');
   static String buildUrlEndpoint(ApiRoute route) => '$baseUrl${route.path}';
+  static String withDynamicSegment(ApiRoute route, String segment) =>
+      '$baseUrl${route.path}$segment';
+  static String withParseValues(ApiRoute route, List<String> values) {
+    String parsedPath = route.path;
+    for (var value in values) {
+      parsedPath = parsedPath.replaceFirst('{v}', value);
+    }
+    return '$baseUrl$parsedPath';
+  }
 }
